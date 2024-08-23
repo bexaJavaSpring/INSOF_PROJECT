52cff2e9772fef4c7cd54feb3a6442ce7b709a0a
behruz <bexruzizzatullayev@gmail.com>
2024-08-24 01:37:55 +0500

    some changes

--- src/main/java/bekhruz/uz/dtos/UserClickRequest.java
+++ src/main/java/bekhruz/uz/dtos/UserClickRequest.java
@@ -2,12 +2,14 @@ package bekhruz.uz.dtos;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
-public class UserClickRequest {
+public class UserClickRequest implements Serializable {
     private String x_count;
     private String y_count;
     private String username;
